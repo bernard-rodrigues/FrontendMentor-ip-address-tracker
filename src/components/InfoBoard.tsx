6c61193b8c5506480d679cb3629363ceec7d9a7a
bernardrodrigues-cead <bernard.rodrigues@uab.ufjf.br>
2023-02-15 15:59:58 -0300

    Changed aria-role from info board

--- src/components/InfoBoard.tsx
+++ src/components/InfoBoard.tsx
@@ -25,11 +25,11 @@ export function InfoBoard(){
     }
 
     return(
-        <div role="feed" className={`
+        <div role="list" className={`
             bg-white flex flex-col text-center justify-between w-[330px] mt-[24px] pt-[25px] pb-[19px] gap-[20px] rounded-2xl z-10
             lg:text-left lg:flex-row lg:min-w-[1110px] lg:min-h-auto lg:py-[33px] lg:mt-[48px] lg:gap-0`}>
             {getFormatted().map((info, index) => (
-                <div key={info.name} className="flex flex-col w-full gap-[4px] lg:px-[33px] lg:gap-1 relative">
+                <div role="listitem" key={info.name} className="flex flex-col w-full gap-[4px] lg:px-[33px] lg:gap-1 relative">
                     <span className="text-darkGray text-[10px] lg:text-[14px] font-myBold">{info.name}</span>
                     <span className="text-veryDarkGray text-[19px] lg:text-[25px] font-mySemiBold">{info.info}</span>
                     {index > 0 ?
